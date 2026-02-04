4cbc135dd5d1dc33da423e7f201e9b1d48bcea27
DanielMaku-STUDENT <g00467390@atu.ie>
2026-02-04 16:28:51 +0000

    start

--- src/main/java/ie/atu/sem2week2/Sem2week2Application.java
+++ src/main/java/ie/atu/sem2week2/Sem2week2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sem2week2Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Sem2week2Application.class, args);
     }
